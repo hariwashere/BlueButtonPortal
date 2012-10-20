9523875fa8eac0993f5520f1fff2aaca08e64316
Shrihari <smurlidharan3@gatech.edu>
2012-10-20 00:06:36 -0400

    Shrihari:Created a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueButtonPortal::Application.config.secret_token = '615a13eed77666fc958e2b498c38c6b86e5240ecde4eac671a832a3384cae630629bd521157730ba602ea56a48ba2e27bd65b8ef1d1ea415ed5d8f350f10b61b'
