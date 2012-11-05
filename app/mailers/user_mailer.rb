bcc81f99d12eb83894052d90ea8b738b64f38207
Shrihari <smurlidharan3@gatech.edu>
2012-11-05 01:17:47 -0500

    Shrihari:Changed the mail subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
   default from: "bluebuttonportal@gmail.com"
   def bbp_pull_response(bbp_pull_request)
     @uuid = bbp_pull_request.uuid
-    mail(:to => bbp_pull_request.email, :subject => "A test message from the app")
+    mail(:to => bbp_pull_request.email, :subject => "Blue button pull request")
   end
 end
