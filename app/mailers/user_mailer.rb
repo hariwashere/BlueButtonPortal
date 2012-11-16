2e8bea795c4f8b8fb769a1530445eeda3ed121bf
Shrihari <smurlidharan3@gatech.edu>
2012-11-15 23:09:43 -0500

    Shrihari:Communication happens through the direct email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "bluebuttonportal@gmail.com"
+  default from: "shari@direct.i3l.gatech.edu"
   def bbp_pull_response(bbp_pull_request)
     @uuid = bbp_pull_request.uuid
     mail(:to => bbp_pull_request.email, :subject => "Blue button pull request")
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,21 +1,24 @@
 ActionMailer::Base.delivery_method = :smtp
 
-ActionMailer::Base.smtp_settings = {
-:enable_starttls_auto => true,
-:address => 'smtp.gmail.com',
-:port => 587,
-:domain => "gmail.com",
-:user_name => 'bluebuttonportal',
-:password => 'bluebutton',
-:authentication => 'plain'
-}
-
 #ActionMailer::Base.smtp_settings = {
 #:enable_starttls_auto => true,
-#:address => 'mail.i3l.gatech.edu',
-#:port => 465,
-#:domain => "direct.i3l.gatech.edu",
-#:user_name => 'shari',
-#:password => 'shrihari',
-#:authentication => 'login'
+#:address => 'smtp.gmail.com',
+#:port => 587,
+#:domain => "gmail.com",
+#:user_name => 'bluebuttonportal',
+#:password => 'bluebutton',
+#:authentication => 'plain'
 #}
+#ActionMailer::Base.perform_deliveries = true
+#ActionMailer::Base.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+:enable_starttls_auto => true,
+:address => 'mail.i3l.gatech.edu',
+:port => 25,
+#:domain => "direct.i3l.gatech.edu",
+:user_name => 'shari',
+:password => 'shrihari',
+:authentication => :plain
+#:openssl_verify_mode => :none
+}
