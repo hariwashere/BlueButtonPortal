129c9fae517043aa44513f1f738f3d88c7353e14
Shrihari <smurlidharan3@gatech.edu>
2012-11-16 01:28:21 -0500

    Shrihari:Request to EHR now has an xml attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,13 @@ class UserMailer < ActionMailer::Base
 
   def ehr_pull_request(patient_detail)
     @patient_detail = patient_detail
+    attachments['patient_detail.xml'] = create_attachment(patient_detail)
     mail(:to => @patient_detail.destination_email, :subject => "Patient details pull request")
+  end
+
+  def create_attachment(patient_detail)
+    file_name = "app/views/user_mailer/attachments/attachment_" + patient_detail.first_name + ".xml"
+    File.write(file_name, patient_detail.to_xml(:except => [:id]))
+    File.read(file_name)
   end
 end
